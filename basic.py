c646edea2612a5f964d057a7aa5923662d8340d2
sandeep kumar chaurasia <58679551+sandeepkrc@users.noreply.github.com>
2023-12-10 13:47:17 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,4 +1,4 @@
-def fnn():
+def send_email():
     """
     function for sending email to
     team every day
@@ -6,7 +6,7 @@ def fnn():
     """
     print("function running due to the schudler")
     pass
-def fdelete():
+def delete_user():
     """
     function for 
     after 30 day free trail
@@ -18,4 +18,6 @@ def fdelete():
 from apscheduler.schedulers.background import BackgroundScheduler
 scheduler = BackgroundScheduler()
 scheduler.add_job(fnn, 'interval', seconds=10)
+# scheduler.add_job(function_name,'interval',seconds= 40,args=[argument1_of_function,argument2_of_function])
+# scheduler.add_job(function_name,args=[argument1_of_function,argument2_of_function],trigger='cron', minute=1)
 scheduler.start()
